b6e5df4fc000871c92d88367539889c3174f9a46
jibingbing <519974770@qq.com>
2018-08-28 15:14:01 +0800

    canal client demo

---
+++ src/main/java/com/alibaba/otter/App.java
@@ -0,0 +1,16 @@
+package com.alibaba.otter;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ src/main/java/com/alibaba/otter/canal/SimpleCanalClientExample.java
@@ -0,0 +1,93 @@
+package com.alibaba.otter.canal;
+
+import com.alibaba.otter.canal.client.CanalConnector;
+import com.alibaba.otter.canal.client.CanalConnectors;
+import com.alibaba.otter.canal.common.utils.AddressUtils;
+import com.alibaba.otter.canal.protocol.CanalEntry;
+import com.alibaba.otter.canal.protocol.Message;
+import com.google.protobuf.InvalidProtocolBufferException;
+
+import java.net.InetSocketAddress;
+import java.util.List;
+
+public class SimpleCanalClientExample {
+    public static void main(String[] args) {
+        CanalConnector connector = CanalConnectors.newSingleConnector(new InetSocketAddress(AddressUtils.getHostIp(), 11111),"example", "", "");
+
+        try {
+            int batchSize = 1000;
+            int emptyCount = 0;
+            connector.connect();
+            connector.subscribe(".*\\..*");
+            connector.rollback();
+            int totalEmptyCount = 120;
+            while (emptyCount < totalEmptyCount) {
+                Message message = connector.getWithoutAck(batchSize);
+                long batchId = message.getId();
+                int size = message.getEntries().size();
+                if (batchId == -1 || size == 0) {
+                    emptyCount++;
+                    System.out.println("emptyCount: " + emptyCount);
+                    try {
+                        Thread.sleep(1000);
+                    } catch (InterruptedException e) {
+                        e.printStackTrace();
+                    }
+                } else {
+                    emptyCount = 0;
+                    printEntry(message.getEntries());
+                }
+
+                connector.ack(batchId);
+            }
+        } finally {
+            connector.disconnect();
+        }
+    }
+
+    private static void printEntry(List<CanalEntry.Entry> entries) {
+        for (CanalEntry.Entry entry : entries) {
+            if (entry.getEntryType() == CanalEntry.EntryType.TRANSACTIONBEGIN || entry.getEntryType() == CanalEntry.EntryType.TRANSACTIONEND) {
+                continue;
+            }
+
+            CanalEntry.RowChange rowChange;
+            try {
+                rowChange = CanalEntry.RowChange.parseFrom(entry.getStoreValue());
+            } catch (InvalidProtocolBufferException e) {
+                throw new RuntimeException("ERROR ## parser of eromanga-event has an error , data:" + entry.toString(), e);
+            }
+
+            CanalEntry.EventType eventType = rowChange.getEventType();
+            System.out.println(String.format("================&gt; binlog[%s:%s] , name[%s,%s] , eventType : %s",
+                    entry.getHeader().getLogfileName(), entry.getHeader().getLogfileOffset(),
+                    entry.getHeader().getSchemaName(), entry.getHeader().getTableName(),
+                    eventType));
+
+            if (eventType == CanalEntry.EventType.QUERY || rowChange.getIsDdl()) {
+                System.out.println("sql----->" + rowChange.getSql());
+            }
+
+            for (CanalEntry.RowData rowData : rowChange.getRowDatasList()) {
+                if (eventType == CanalEntry.EventType.DELETE) {
+                    printColumn(rowData.getBeforeColumnsList());
+                } else if (eventType == CanalEntry.EventType.INSERT) {
+                    printColumn(rowData.getAfterColumnsList());
+                } else if (eventType == CanalEntry.EventType.UPDATE) {
+                    printColumn(rowData.getAfterColumnsList());
+                } else {
+                    System.out.println("-------&gt; before");
+                    printColumn(rowData.getBeforeColumnsList());
+                    System.out.println("-------&gt; after");
+                    printColumn(rowData.getAfterColumnsList());
+                }
+            }
+        }
+    }
+
+    private static void printColumn(List<CanalEntry.Column> columns) {
+        for (CanalEntry.Column column : columns) {
+            System.out.println(column.getName() + " : " + column.getValue() + "    update=" + column.getUpdated());
+        }
+    }
+}
